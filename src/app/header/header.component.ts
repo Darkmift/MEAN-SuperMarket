5e1ba9ed1c03249a60c15123c488dddbc59f89bd
dark_mift <darkmift@gmail.com>
2019-12-16 12:48:12 +0200

    header nav menu display added isauthenticated

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  userIsAuthenticated = false;
+  private authListenerSubs: Subscription;
+
+  constructor(private authService: AuthService) { }
+
 
   ngOnInit() {
+    this.userIsAuthenticated = this.authService.getIsAuth();
+    this.authListenerSubs = this.authService.getAuthStatusListener().subscribe((isAuthenticated: boolean) => {
+      this.userIsAuthenticated = isAuthenticated;
+    });
+  }
+
+  onLogOut() {
+    this.authService.logOut();
+  }
+
+  ngOnDestroy() {
+    this.authListenerSubs.unsubscribe();
   }
 
 }
