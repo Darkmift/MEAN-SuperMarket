b2efbb5b54f33bb64bb29099361bb6b9426224b6
dark_mift <darkmift@gmail.com>
2019-12-20 22:32:10 +0200

    more work on portal elemnts.

--- src/app/shop/portal/portal.component.ts
+++ src/app/shop/portal/portal.component.ts
@@ -1,34 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { AuthService } from 'src/app/auth/auth.service';
+import { AuthService } from '../../auth/auth.service';
+import { User } from '../../auth/models/user.model';
 
 @Component({
   selector: 'app-portal',
   templateUrl: './portal.component.html',
   styleUrls: ['./portal.component.css']
 })
-export class PortalComponent implements OnInit {
+export class PortalComponent implements OnInit, OnDestroy {
 
 
-  isLoading = false;
+  isLoading = true;
   private authStatusSub: Subscription;
   userIsAuthenticated = false;
   userId: string;
   role: boolean;
+  user: User;
+  hasPreviousCart: boolean;
+  imagePath: string;
 
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
-
-    this.isLoading = true;
+    this.imagePath = 'http://localhost:3000/public/images/grumpy.jpg';
+    this.isLoading = false;
     this.userId = this.authService.getUserId();
     this.role = this.authService.getRole();
     this.userIsAuthenticated = this.authService.getIsAuth();
-    this.authStatusSub = this.authService.getAuthStatusListener().subscribe((isAuthenticated: boolean) => {
-      this.userIsAuthenticated = isAuthenticated;
-      this.userId = this.authService.getUserId();
-      this.role = this.authService.getRole();
-    });
+    this.user = this.authService.getUser();
+    this.hasPreviousCart = false;
+    this.authStatusSub = this.authService.getAuthStatusListener()
+      .subscribe((isAuthenticated: boolean) => {
+        this.isLoading = false;
+        this.userIsAuthenticated = isAuthenticated;
+        this.userId = this.authService.getUserId();
+        this.role = this.authService.getRole();
+        this.user = this.authService.getUser();
+        console.log('TCL: PortalComponent -> ngOnInit -> this.user', this.user);
+      }, err => {
+        console.log('TCL: PortalComponent -> ngOnInit -> err', err);
+      });
+  }
+
+  capitalize = (s) => {
+    if (typeof s !== 'string') {
+      return '';
+    }
+    return s.charAt(0).toUpperCase() + s.slice(1);
+  };
+
+  ngOnDestroy(): void {
+    this.authStatusSub.unsubscribe();
+
   }
 
 }
